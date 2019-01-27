22bfe81b4c975a0747e24af6c07021ccde6df70d
Yonnick Brown <25hour24@gmail.com>
2019-01-27 18:27:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  code
+def batch_badge_creator(name_array)
+  name_array.map do |name|
+    "Hello, my name is #{name}."
+  end
 end
